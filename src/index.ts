881d26d5ed524d6e7be949b556adf44a8eaeaacd
nchen <chenrong2003@hotmail.com>
2018-11-24 01:55:18 +0800

    Set ordering of the documents before generating PDF

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,35 @@ import createGithubClient, { GithubClient } from './github';
 import createFiles, { Files } from './files';
 import makePdf from './pdf';
 
+const fileNames = `basic-types.md
+variable-declarations.md
+interfaces.md
+classes.md
+functions.md
+generics.md
+enums.md
+type-inference.md
+type-compatibility.md
+advanced-types.md
+symbols.md
+iterators-and-generators.md
+modules.md
+namespaces.md
+namespaces-and-modules.md
+module-resolution.md
+declaration-merging.md
+jsx.md
+decorators.md
+mixins.md
+triple-slash-directives.md
+type-checking-javascript-files.md`.split("\n");
+
+function getIndex(fname: string): number {
+    let f = fname.split("/").pop() !;
+    f = f.split(" ").join("-").toLowerCase();
+    return fileNames.indexOf(f);
+}
+
 (async () => {
     const cwd = process.cwd();
     const githubClient: GithubClient = createGithubClient(execa);
@@ -16,7 +45,18 @@ import makePdf from './pdf';
     const clonedRepoDir: string = 
         await githubClient.clone('Microsoft/TypeScript-Handbook', path.join(cwd, 'temp'));
     log('Done');
-    const markdownFiles: string[] = files.findMarkdown(path.join(clonedRepoDir, 'pages'));
+    let markdownFiles: string[] = files.findMarkdown(path.join(clonedRepoDir, 'pages'));
+    markdownFiles = markdownFiles.sort(function(a: string, b: string) : number {
+        let ia = getIndex(a);
+        let ib = getIndex(b);
+
+        if (ia > ib) return 1;
+        if (ia < ib) return -1;
+        return 0;
+    });
+
+    markdownFiles = markdownFiles.filter(value => getIndex(value) > -1);
+
     log('Creating pdf...');
     await makePdf(mpdf().concat, markdownFiles, path.join(cwd, 'handbook.pdf'));
     log('Done');
